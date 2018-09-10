80f0dddcf4e9acb0af50781a1a5f32866d738a28
Mannavard <mannavard.ak@gmail.com>
2018-09-10 18:14:25 +0300

    pre finish

---
+++ src/app/product-image/product-image.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, HostBinding, OnInit } from '@angular/core';
+import { Product } from './../product.model';
+
+@Component({
+  selector: 'app-product-image',
+  template: `
+  <img class="product-image" [src]="product.imageUrl">
+  `
+})
+export class ProductImageComponent implements OnInit {
+  @Input() product: Product;
+  @HostBinding('attr.class') cssClass = 'ui small image';
+  ngOnInit() {
+  }
+
+}
